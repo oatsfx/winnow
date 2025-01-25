457c398dbee9ade1059e4f3c517a0a19232f9ccb
oatsfx <30326975+oatsfx@users.noreply.github.com>
2025-01-25 11:50:07 -0700

    fix: foundation link

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -4,7 +4,7 @@ const Banner: React.FC = () => {
   return (
     <div className="bg-primary/10 items-center text-center p-1 w-full bg-gradient-to-r from-primary via-secondary to-primary bg-[length:200%_auto] animate-gradient">
       <Link
-        to="https://tilt.fyi/aLMXqD3aLy"
+        to="https://donate.tiltify.com/10f59dcf-430a-49c6-9698-beb42a03c2e2"
         target="_blank"
         className="text-accent shadow-sm underline hover:text-white"
       >
