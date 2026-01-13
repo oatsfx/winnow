1e74c8b7b6801489f9fc4f29e51ce51c7d90ccfb
oatsfx <30326975+oatsfx@users.noreply.github.com>
2026-01-12 20:04:22 -0700

    Update Banner.tsx

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -4,7 +4,7 @@ const Banner: React.FC = () => {
   return (
     <div className="bg-primary/10 items-center text-center p-1 w-full bg-gradient-to-r from-primary via-secondary to-primary bg-[length:200%_auto] animate-gradient">
       <Link
-        to="https://donate.tiltify.com/10f59dcf-430a-49c6-9698-beb42a03c2e2"
+        to="https://donate.tiltify.com/a5b1a739-e5e6-4cc7-ab86-c2d4bbbd0f2d/incentives"
         target="_blank"
         className="text-accent shadow-sm underline hover:text-white"
       >
